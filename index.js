29ffacbdd5b15227e08e176ee6baf3c36e3abcd9
Nitin Kumar <83333586+nitinriaan@users.noreply.github.com>
2022-07-30 12:24:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+var randomNumber1 = Math.floor(Math.random()*6) + 1; //1 to 6
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png to dice6.png
+
+var randomImageSource = "images/" + randomDiceImage; //from images foleder
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+//dice 2
+
+var randomNumber2 = Math.floor(Math.random()*6) + 1;
+
+var randomDiceImage2 = "dice" + randomNumber2 + ".png"; //dice1.png to dice6.png
+
+var randomImageSource2 = "images/" + randomDiceImage2; //from images foleder
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src", randomImageSource2);
+
+if(randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 wins";
+}
+else if(randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 wins";
+}
+else{
+    document.querySelector("h1").innerHTML = "Draw";
+}
